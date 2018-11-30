4cd99386f3e97b6eb8eff6e41e371f69c5024a2f
NekoSaiKou <ricky0711.yang@gmail.com>
2018-11-30 22:48:27 +0800

    Add bamboorat voca when setting volume

---
+++ js/setting.js
@@ -0,0 +1,7 @@
+var volume_slider = document.getElementById("volume_slider");
+var bamboorat1_voice = document.getElementById("bamboorat1");
+
+volume_slider.oninput = function() {
+	bamboorat1_voice.volume = volume_slider.value/100;
+	bamboorat1_voice.play();
+}
