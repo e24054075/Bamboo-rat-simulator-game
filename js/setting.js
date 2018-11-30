cf649f077151ac42ad0d7f299715811a51c467db
NekoSaiKou <ricky0711.yang@gmail.com>
2018-11-30 23:00:30 +0800

    Modify volume setting. Play after dragging is finished.

--- js/setting.js
+++ js/setting.js
@@ -2,6 +2,7 @@ var volume_slider = document.getElementById("volume_slider");
 var bamboorat1_voice = document.getElementById("bamboorat1");
 
 volume_slider.oninput = function() {
+	bamboorat1_voice.load();
 	bamboorat1_voice.volume = volume_slider.value/100;
 	bamboorat1_voice.play();
 }
