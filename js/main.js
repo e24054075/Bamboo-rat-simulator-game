975115c1f5160837a25b39ed380c4ecb2a233f60
e24054075 <seastar90763@gmail.com>
2019-01-08 20:54:37 +0800

    fix spin

--- js/main.js
+++ js/main.js
@@ -66,7 +66,7 @@ var mainpage ={
 	game.load.spritesheet('rat_player','assets/img/rat4.png', 210, 114);
 	game.load.spritesheet('hat','assets/img/hat.png', 84, 84);
 	game.load.image("wheel", "./assets/img/plat.png");
-	game.load.image("pin", "./assets/img/spin.png");     
+	game.load.image("pin", "./assets/img/spin3.png");     
 	},
   create:()=>{	
     $('#day').text("DAY "+day);
@@ -117,9 +117,9 @@ var mainpage ={
 	wheel.scale.set(0.5);
     wheel.anchor.set(0.5);
 	wheel.visible = false;
-    pin = game.add.sprite(209, 490, "pin");
+    pin = game.add.sprite(208.5, 490, "pin");
     pin.anchor.set(0.5);
-	pin.scale.set(1.1);
+	pin.scale.set(1.6);
 	pin.visible = false;
 	//按鈕設定
 	this.button_eat = game.add.button(150, 640, 'bowl');
