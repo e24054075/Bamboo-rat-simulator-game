3cc66e408e3a08a1aba1c2fd0bee8999606896ec
danzojarvis <43299095+danzojarvis@users.noreply.github.com>
2019-01-09 00:34:07 +0800

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -1,9 +1,11 @@
 var volume_slider = document.getElementById("volume_slider");
 var bamboorat1_voice = document.getElementById("bamboorat1");
-
+var bamboorat2_voice = document.getElementById("bamboorat1");
+bamboorat2_voice.autoplay();
 volume_slider.oninput = function() {
 	//bamboorat1_voice.load();
 	bamboorat1_voice.volume = volume_slider.value/100;
+	bamboorat2_voice.volume = volume_slider.value/100;
 	bamboorat1_voice.play();
 }
 
